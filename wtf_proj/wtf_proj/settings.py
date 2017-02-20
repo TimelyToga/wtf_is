9a5e9bbcfb90ab0078e9472eb297647ea540b9cf
TimelyToga <tblumberg39@gmail.com>
2017-02-20 15:49:48 -0500

    Tweaked some shit

--- wtf_proj/wtf_proj/settings.py
+++ wtf_proj/wtf_proj/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 # Via Whitenoise
 
-STATIC_URL = '/static/'
+STATIC_HOST = '' if not DEBUG else ''
+STATIC_URL = STATIC_HOST + '/static/'
 STATIC_ROOT = join(BASE_DIR, "staticfiles")
 WHITENOISE_ROOT = join(BASE_DIR, "basic_files")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
